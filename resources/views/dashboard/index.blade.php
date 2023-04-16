7d02d1cd24c441c6fb4e3a4b802183caf52c513c
unknown <you@example.com>
2023-04-16 18:04:20 +0500

    designm

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.dashboard')
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <hr style="height: 2px; background: black">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3">
+
+
+    <div class="row mt-5">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Total Empolyee</h5>
@@ -15,77 +12,106 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Total Customer</h5>
-
-
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <hr style="height: 2px; background: black">
+        </div>
+        <div class="row">
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Total Sale</h5>
-
-
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Monthly Target</h5>
-
+                    <h5 class="card-title"> Total purcahse price</h5>
+                </div>
+            </div>
+        </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"> Total profit</h5>
+                    </div>
                 </div>
             </div>
         </div>
 
-    </div>
+
     <div class="row">
         <div class="col-12">
             <hr style="height: 2px; background: black">
         </div>
     </div>
     <div class="row">
-        <div class="col-md-3">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Expense</h5>
-
-
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Total Expense</h5>
-
-
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Today Sale</h5>
-
-
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Total Purches</h5>
-
+                    <h5 class="card-title">Today Purchase</h5>
                 </div>
             </div>
         </div>
 
-    </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Today Profit</h5>
+                </div>
+            </div>
+        </div>
+        <div>
 
+            <div class="row">
+                <div class="col-12">
+                    <hr style="height: 2px; background: black">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Expense</h5>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Refunds</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Partial Payments</h5>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+       
 
 @endsection
